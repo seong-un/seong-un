ab7bb997f8f433079a00573bc843fac8249da335
seong-un <122589861+seong-un@users.noreply.github.com>
2024-01-10 16:22:42 +0900

    [Silver I] Title: 카드 구매하기, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+N = int(input())
+Pi = list(map(int, input().split()))
+Pi = [0] + Pi
+dp = [0 for i in range(N+1)]
+
+for i in range(1, N+1):
+    a = i
+    for j in range(i, -1, -1):
+        dp[i] = max(dp[i], Pi[j] + dp[i - j])
+
+print(dp[-1])
