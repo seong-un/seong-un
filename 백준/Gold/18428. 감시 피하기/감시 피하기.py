690b97280f5d003b10cd8eb02079424fe94d56ec
seong-un <122589861+seong-un@users.noreply.github.com>
2024-01-17 17:49:40 +0900

    [Gold V] Title: 감시 피하기, Time: 144 ms, Memory: 111276 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -0,0 +1,52 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+N = int(input())
+corrider = [list(input().split()) for i in range(N)]
+
+list_X = []
+for i in range(N):
+    for j in range(N):
+        if corrider[i][j] == 'X':
+            list_X.append((i, j))
+
+for_yes = False
+for oo in list(combinations(list_X, 3)):
+    encounter = False
+    for o in oo:
+        corrider[o[0]][o[1]] = 'O'
+    for i in range(N):
+        for j in range(N):
+            if corrider[i][j] != 'T':
+                continue
+
+            for k in range(4):
+                a = 1
+                while True:
+                    x = i + dx[k] * a
+                    y = j + dy[k] * a
+                    if x < 0 or y < 0 or x >= N or y >= N or corrider[x][y] in ['T', 'O']:
+                        break
+                    if corrider[x][y] == 'S':
+                        for o in oo:
+                            corrider[o[0]][o[1]] = 'X'
+                        encounter = True
+                        break
+                    a += 1
+                if encounter:
+                    break
+            if encounter:
+                break
+        if encounter:
+            break
+    if not encounter:
+        print('YES')
+        for_yes = True
+        break
+
+if not for_yes:
+    print('NO')
