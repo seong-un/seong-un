0f32678b1a2db57562c5e7bf98138c962def3117
seong-un <122589861+seong-un@users.noreply.github.com>
2024-01-20 15:35:57 +0900

    [Silver III] Title: 1, Time: 4052 ms, Memory: 118736 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,15 @@
+import sys
+sys.set_int_max_str_digits(100000)
+input = sys.stdin.readline
+
+while True:
+    try:
+        result = 1
+        n = int(input())
+        while True:
+            if result % n == 0:
+                print(len(str(result)))
+                break
+            result = int(str(result) + '1')
+    except:
+        break
