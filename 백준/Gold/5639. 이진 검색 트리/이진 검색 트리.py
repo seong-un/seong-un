8d5854c519010c1df94bb5ee0aac7caff0f537da
seong-un <122589861+seong-un@users.noreply.github.com>
2024-02-27 14:07:46 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2928 ms, Memory: 424688 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -3,10 +3,8 @@ sys.setrecursionlimit(2*10**5)
 input = sys.stdin.readline
 
 def front_to_back(tree):
-    if len(tree) == 1:
-        return [tree[0]]
-    elif len(tree) == 0:
-        return []
+    if len(tree) in [0, 1]:
+        return tree
     root = tree[0]
     for i in range(1, len(tree)):
         if tree[i] > root:
