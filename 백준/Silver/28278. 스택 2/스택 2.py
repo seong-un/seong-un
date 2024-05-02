80fecd832a6e1e8266dfcef3b7372828650d75ec
seong-un <122589861+seong-un@users.noreply.github.com>
2024-05-02 14:26:42 +0900

    [Silver IV] Title: 스택 2, Time: 856 ms, Memory: 102856 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -3,29 +3,26 @@ input = sys.stdin.readline
 
 N = int(input())
 
-stack = [0] * 1000000
-i = 0
-for _ in range(N):
-    command = list(map(int, input().split()))
+stack = []
+for i in range(N):
+    command = input().split()
 
-    if command[0] == 1:
-        stack[i] = command[1]
-        i += 1
-    elif command[0] == 2:
-        if i == 0:
-            print(-1)
+    if command[0] == '1':
+        stack.append(command[1])
+    elif command[0] == '2':
+        if stack:
+            print(stack.pop())
         else:
-            i -= 1
-            print(stack[i])
-    elif command[0] == 3:
-        print(i)
-    elif command[0] == 4:
-        if i == 0:
+            print(-1)
+    elif command[0] == '3':
+        print(len(stack))
+    elif command[0] == '4':
+        if stack == []:
             print(1)
         else:
             print(0)
     else:
-        if i == 0:
-            print(-1)
+        if stack:
+            print(stack[-1])
         else:
-            print(stack[i-1])
+            print(-1)
