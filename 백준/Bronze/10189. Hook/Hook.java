fd4d0d7f2a0e9768386573760cd22c8a55ce2552
seong-un <122589861+seong-un@users.noreply.github.com>
2025-03-15 16:31:56 +0900

    [Bronze V] Title: Hook, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+	}
+}
