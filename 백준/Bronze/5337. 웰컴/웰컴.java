853a828cc258c86641f1b92c1cd376636300f34a
seong-un <122589861+seong-un@users.noreply.github.com>
2025-03-15 16:15:33 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
