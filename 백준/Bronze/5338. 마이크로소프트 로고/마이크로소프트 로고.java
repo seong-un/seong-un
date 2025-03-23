8b77273891e8dbec5711e9809b1591fb6b6dd8f8
seong-un <122589861+seong-un@users.noreply.github.com>
2025-03-23 18:00:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,9 @@
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
